6b28f5906b4d082ecd8ef9fd4fd3ab116ae1311f
67Konstantin <113464840+67Konstantin@users.noreply.github.com>
2023-11-10 15:34:30 +0300

    *

--- lib/features/maps/view/about_route_screen.dart
+++ lib/features/maps/view/about_route_screen.dart
@@ -1,5 +1,8 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:smolathon_mobile/widgets/exports.dart';
+
+import '../../../help_classes/exports.dart';
 
 @RoutePage()
 class AboutRouteScreen extends StatefulWidget {
@@ -10,8 +13,28 @@ class AboutRouteScreen extends StatefulWidget {
 }
 
 class _AboutRouteScreenState extends State<AboutRouteScreen> {
+  Widget _body(var heigth, var width) {
+    return Center();
+  }
+
+  CustomAppBar _appBar() {
+    return CustomAppBar(
+      title: 'Ваш профиль',
+      backgroundColor: MyColor.darkBlue3D5060,
+      iconColor: MyColor.midleGrey7CA3BA,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const SizedBox();
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Colors.blue.shade50,
+      appBar: _appBar(),
+      body: _body(height, width),
+      drawer: ComplexDrawer(),
+    );
   }
+
 }
--- lib/features/maps/view/point_screen.dart
+++ lib/features/maps/view/point_screen.dart
@@ -1,5 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:smolathon_mobile/help_classes/exports.dart';
+import 'package:smolathon_mobile/widgets/exports.dart';
 
 @RoutePage()
 class PointScreen extends StatefulWidget {
@@ -10,8 +12,27 @@ class PointScreen extends StatefulWidget {
 }
 
 class _PointScreenState extends State<PointScreen> {
+  Widget _body(var heigth, var width) {
+    return Center();
+  }
+
+  CustomAppBar _appBar() {
+    return CustomAppBar(
+      title: 'Ваш профиль',
+      backgroundColor: MyColor.darkBlue3D5060,
+      iconColor: MyColor.midleGrey7CA3BA,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const SizedBox();
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Colors.blue.shade50,
+      appBar: _appBar(),
+      body: _body(height, width),
+      drawer: ComplexDrawer(),
+    );
   }
 }
